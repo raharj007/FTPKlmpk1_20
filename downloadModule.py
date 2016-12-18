cba4b7b03773d4e8a0ba030985a19f8f9eab2dc4
Panji Rimawan <Panji Rimawan>
2016-12-18 20:20:14 +0700

    tambah modul download

---
+++ downloadModule.py
@@ -0,0 +1,15 @@
+import os
+import pathconverter
+
+CRLF="\r\n"
+
+def download(socket_client, currentDirectory, path):
+	joinPath = os.path.join(currentDirectory, path)
+	if os.path.exists(joinPath):
+		filesize=os.path.getsize(path)
+		socket_client.sendall(str(filesize)+"\r\n"+"\r\n")
+		with open(path,"rb") as file:
+			data = file.read()
+			socket_client.sendall(data)
+	else:
+		socket_client.sendall("\r\n")
