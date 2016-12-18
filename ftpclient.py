c45c61fb3641d77ad7f226baf87376a3aff51196
Panji Rimawan <Panji Rimawan>
2016-12-18 20:20:38 +0700

    wqtambah handle command retr

--- ftpclient.py
+++ ftpclient.py
@@ -2,6 +2,8 @@ import socket
 import select
 import sys
 
+BUFF=1024
+
 socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_client.connect(('localhost', 5000))
 
@@ -18,7 +20,25 @@ while True:
     try:
         cmd=raw_input()
         socket_client.sendall(cmd+"\r\n")
-        print socket_client.recv(1024)
+        if cmd.partition(" ")[0].upper()=="RETR":
+            filesize=0
+            filereceived=0
+            data=""
+            temp=socket_client.recv(BUFF)
+            filesize, delimiter, body=temp.partition("\r\n\r\n")
+            data=data+body
+            #print temp.partition("\r\n\r\n")
+            filereceived=len(data)
+            #print "filereceived: " + str(filereceived)
+            #print "filesize: " + str(filesize)
+            with open(cmd.partition(" ")[2], 'wb') as frcvd:
+                while int(filereceived)<int(filesize):
+                    temp=socket_client.recv(BUFF)
+                    frcvd.write(temp)
+                    filereceived+=len(temp)
+                frcvd.close()
+        else:
+            print socket_client.recv(1024)
 
     except socket.error, exc:
         socket_client.close()
