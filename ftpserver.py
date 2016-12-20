c0c129197ec2a415c424ce545ca7f6ff142bca67
Panji Rimawan <Panji Rimawan>
2016-12-20 17:26:46 +0700

    ganti ip ke lokal

--- ftpserver.py
+++ ftpserver.py
@@ -5,7 +5,7 @@ from ClientHandler import ClientHandler
 
 port=5000
 
-server_address = ('10.181.1.207', port)
+server_address = ('127.0.0.1', port)
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind(server_address)
