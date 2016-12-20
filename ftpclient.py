2669fcb6e0bfe69b1b2aa7941023f86cb0b45b36
Panji Rimawan <Panji Rimawan>
2016-12-20 12:52:29 +0700

    edit download mode ketika file not exists

--- ftpclient.py
+++ ftpclient.py
@@ -7,7 +7,7 @@ CRLF="\r\n"
 BUFF=1024
 
 socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket_client.connect(('192.168.1.3', 5000))
+socket_client.connect(('127.0.0.1', 5000))
 
 #commands = ['USER raharjo\r\n', 'PASS 123\r\n', 'PWD\r\n', 'QUIT\r\n']
 
@@ -22,23 +22,26 @@ while True:
     try:
         cmd=raw_input()
         if cmd.partition(" ")[0].upper()=="RETR":
-            filesize=0
-            filereceived=0
-            data=""
             socket_client.sendall(cmd+'\r\n')
             temp=socket_client.recv(BUFF)
-            filesize, delimiter, body=temp.partition("\r\n\r\n")
-            filedownload=cmd.partition(" ")[2]
-            with open(filedownload, 'wb') as frcvd:
-                data=data+body
-                filereceived=len(data)
-                frcvd.write(data)
-                while int(filereceived)<int(filesize):
-                    temp=socket_client.recv(BUFF)
-                    frcvd.write(temp)
-                    filereceived+=len(temp)
-                frcvd.close()
-            print str(filedownload) + " downloaded." + "\r\n"
+            if temp==" ":
+                print "550 File not found"
+            else:
+                filesize=0
+                filereceived=0
+                data=""
+                filesize, delimiter, body=temp.partition("\r\n\r\n")
+                filedownload=cmd.partition(" ")[2]
+                with open(filedownload, 'wb') as frcvd:
+                    data=data+body
+                    filereceived=len(data)
+                    frcvd.write(data)
+                    while int(filereceived)<int(filesize):
+                        temp=socket_client.recv(BUFF)
+                        frcvd.write(temp)
+                        filereceived+=len(temp)
+                    frcvd.close()
+                print str(filedownload) + " downloaded."
         elif cmd.partition(" ")[0].upper()=="STOR":
             filename = cmd.partition(" ")[2]
             if os.path.exists(filename):
@@ -48,7 +51,7 @@ while True:
                     socket_client.sendall(cmd + " " + str(fileupload_size) + " " + "\r\n\r\n" + dataupload)
                 print str(filename) + " uploaded."
             else:
-                print "550 File not found" + "\r\n"
+                print "550 File not found"
         else:
             socket_client.sendall(cmd+"\r\n")
             print socket_client.recv(1024).rstrip(CRLF)
